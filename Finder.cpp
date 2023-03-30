978dbff45f1985f685bccc20722d9cf76ae9aaa9
tuanminh.nguyen@student.adelaide.edu.au <tuanminh.nguyen@student.adelaide.edu.au>
2023-03-30 17:23:05 +1030

    add

---
+++ Finder.cpp
@@ -0,0 +1,31 @@
+#include "Finder-1.h"
+
+    using namespace std;
+
+    vector<int> Finder::findSubstrings(string s1, string s2) {
+
+        vector<int> result;
+        string curSubstr = "";
+        for(size_t i = 0; i < s2.size(); i++) {
+            curSubstr += s2[i];
+            size_t found = s1.find(curSubstr);            
+            if (found != string::npos) {
+                result.push_back(found);
+            } else {
+                result.push_back(-1);
+            }
+        }
+        return result;
+    }
+
+int main () {
+    
+        Finder finder;
+        vector<int> result = finder.findSubstrings("4634554567", "45689");
+        for (auto i : result) {
+            cout << i << endl;
+        }
+        return 0;
+}
+
+   

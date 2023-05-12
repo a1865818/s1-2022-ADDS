6d786e60a2219f7e3e041ec85576f4706618c7d1
tuanminh.nguyen@student.adelaide.edu.au <tuanminh.nguyen@student.adelaide.edu.au>
2023-05-12 16:51:21 +0930

    add

--- Autocomplete.cpp
+++ Autocomplete.cpp
@@ -1,64 +1,49 @@
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-using namespace std;
+#include "Autocomplete.h"
 
-class TrieNode {
-public:
-    unordered_map<char, TrieNode*> children;
-    bool isEndOfWord;
+TrieNode::TrieNode() {
+    isEndOfWord = false;
+}
 
-    TrieNode() {
-        isEndOfWord = false;
-    }
-};
+Autocomplete::Autocomplete() {
+    root = new TrieNode();
+}
 
-class Autocomplete {
-private:
-    TrieNode* root;
-
-    void insertWord(TrieNode* node, const string& word) {
-        for (char c : word) {
-            if (node->children.find(c) == node->children.end()) {
-                node->children[c] = new TrieNode();
-            }
-            node = node->children[c];
+void Autocomplete::insertWord(TrieNode* node, const std::string& word) {
+    for (char c : word) {
+        if (node->children.find(c) == node->children.end()) {
+            node->children[c] = new TrieNode();
         }
-        node->isEndOfWord = true;
+        node = node->children[c];
     }
+    node->isEndOfWord = true;
+}
 
-    void getAllWords(TrieNode* node, const string& prefix, vector<string>& suggestions) {
-        if (node->isEndOfWord) {
-            suggestions.push_back(prefix);
+void Autocomplete::getAllWords(TrieNode* node, const std::string& prefix, std::vector<std::string>& suggestions) {
+    if (node->isEndOfWord) {
+        suggestions.push_back(prefix);
+    }
+    for (auto& child : node->children) {
+        getAllWords(child.second, prefix + child.first, suggestions);
+    }
+}
+
+void Autocomplete::insert(const std::string& word) {
+    insertWord(root, word);
+}
+
+std::vector<std::string> Autocomplete::getSuggestions(const std::string& partialWord) {
+    std::vector<std::string> suggestions;
+    TrieNode* node = root;
+
+    // Traverse to the node representing the partial word
+    for (char c : partialWord) {
+        if (node->children.find(c) == node->children.end()) {
+            return suggestions;  // No suggestions for the partial word
         }
-        for (auto& child : node->children) {
-            getAllWords(child.second, prefix + child.first, suggestions);
-        }
+        node = node->children[c];
     }
 
-public:
-    Autocomplete() {
-        root = new TrieNode();
-    }
-
-    void insert(const string& word) {
-        insertWord(root, word);
-    }
-
-    vector<string> getSuggestions(const string& partialWord) {
-        vector<string> suggestions;
-        TrieNode* node = root;
-
-        // Traverse to the node representing the partial word
-        for (char c : partialWord) {
-            if (node->children.find(c) == node->children.end()) {
-                return suggestions;  // No suggestions for the partial word
-            }
-            node = node->children[c];
-        }
-
-        // Retrieve all words from the subtree rooted at the node
-        getAllWords(node, partialWord, suggestions);
-        return suggestions;
-    }
-};
+    // Retrieve all words from the subtree rooted at the node
+    getAllWords(node, partialWord, suggestions);
+    return suggestions;
+}
---
+++ Autocomplete.h
@@ -0,0 +1,29 @@
+#ifndef AUTOCOMPLETE_H
+#define AUTOCOMPLETE_H
+
+#include <vector>
+#include <string>
+#include <unordered_map>
+class TrieNode {
+public:
+    std::unordered_map<char, TrieNode*> children;
+    bool isEndOfWord;
+
+    TrieNode();
+
+};
+
+class Autocomplete {
+private:
+    TrieNode* root;
+
+    void insertWord(TrieNode* node, const std::string& word);
+    void getAllWords(TrieNode* node, const std::string& prefix, std::vector<std::string>& suggestions);
+
+public:
+    Autocomplete();
+    void insert(const std::string& word);
+    std::vector<std::string> getSuggestions(const std::string& partialWord);
+};
+
+#endif  // AUTOCOMPLETE_H
